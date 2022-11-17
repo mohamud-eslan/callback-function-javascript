58e4a85790d625b935e9d149a6889cd534d6a352
mohamud-eslan <mohmud141@gmail.com>
2022-11-17 06:08:43 +0300

    async await function added

--- promises.js
+++ promises.js
@@ -1,87 +1,87 @@
-// // let pro = new Promise((resolve, reject) =>{
-// //     let a = 1 + 3;
-// //     if(a == 2){
-// //         resolve('success')
-// //     }else{
-// //         reject('failed')
-// //     }
-// // });
-// // pro.then((message) =>{
-// //     console.log('this is then ' + message)
-// // }).catch((message) => {
-// //     console.log('this is the catch ' + message)
-// // });
-// // const userOne = false;
-// // const userTwo = true;
-// // function tutorialCallback(callback, errorCallback){
-// //     if(userOne){
-// //         errorCallback({
-// //             name: 'user one',
-// //             message: 'tis is wrong buddy'
-// //         })
-// //     }
-// //         else if(userTwo){
-// //             errorCallback({
-// //                 name: 'user two',
-// //                 message: 'this is user two'
-// //             })
-// //         }
-// //         else{
-// //             callback(
-// //                 'everything is working correctly')
-// //         }
-// //     }
-// //    tutorialCallback((message) =>{
-// //     console.log('success ' + message)
-// //    }, (error) => {
-// //     console.log(error.name + ' ' + error.message)
-// //    })
+// let pro = new Promise((resolve, reject) =>{
+//     let a = 1 + 3;
+//     if(a == 2){
+//         resolve('success')
+//     }else{
+//         reject('failed')
+//     }
+// });
+// pro.then((message) =>{
+//     console.log('this is then ' + message)
+// }).catch((message) => {
+//     console.log('this is the catch ' + message)
+// });
+// const userOne = false;
+// const userTwo = true;
+// function tutorialCallback(callback, errorCallback){
+//     if(userOne){
+//         errorCallback({
+//             name: 'user one',
+//             message: 'tis is wrong buddy'
+//         })
+//     }
+//         else if(userTwo){
+//             errorCallback({
+//                 name: 'user two',
+//                 message: 'this is user two'
+//             })
+//         }
+//         else{
+//             callback(
+//                 'everything is working correctly')
+//         }
+//     }
+//    tutorialCallback((message) =>{
+//     console.log('success ' + message)
+//    }, (error) => {
+//     console.log(error.name + ' ' + error.message)
+//    })
 
-// //    function tutorialPromise(){
-// //         return new Promise((resolve, reject) => {
-// //             if(userOne){
-// //                 reject({
-// //                     name: 'user one',
-// //                     message: 'tis is wrong buddy'
-// //                 })
-// //             }
-// //                 else if(userTwo){
-// //                     reject({
-// //                         name: 'user two',
-// //                         message: 'this is user two'
-// //                     })
-// //                 }
-// //                 else{
-// //                     resolve(
-// //                         'everything is working correctly')
-// //                 }
-// //         })
+//    function tutorialPromise(){
+//         return new Promise((resolve, reject) => {
+//             if(userOne){
+//                 reject({
+//                     name: 'user one',
+//                     message: 'tis is wrong buddy'
+//                 })
+//             }
+//                 else if(userTwo){
+//                     reject({
+//                         name: 'user two',
+//                         message: 'this is user two'
+//                     })
+//                 }
+//                 else{
+//                     resolve(
+//                         'everything is working correctly')
+//                 }
+//         })
     
-// //     }
-// //     tutorialPromise().then((message) =>{
-// //         console.log('success ' + message)
-// //        }) .catch((error) => {
-// //         console.log(error.name + ' ' + error.message)
-// //        });
-
-//        const doNewChallenge = new Promise((resolve, reject) => {
-//         resolve('challenge one completed');
+//     }
+//     tutorialPromise().then((message) =>{
+//         console.log('success ' + message)
+//        }) .catch((error) => {
+//         console.log(error.name + ' ' + error.message)
 //        });
+
+       // const doNewChallenge = new Promise((resolve, reject) => {
+       //  resolve('challenge one completed');
+       // });
        
-//        const doChallengeTwo = new Promise((resolve, reject) => {
-//         resolve('challenge two completed');
-//        });
+       // const doChallengeTwo = new Promise((resolve, reject) => {
+       //  resolve('challenge two completed');
+       // });
 
-//        const doChallengeThree = new Promise((resolve, reject) => {
-//         resolve('challenge three completed');
-//        });
-//        Promise.race([
-//         doNewChallenge,
-//         doChallengeTwo,
-//         doChallengeThree
-//        ]).then((message) => {
-//         console.log(message);
-//        })
+       // const doChallengeThree = new Promise((resolve, reject) => {
+       //  resolve('challenge three completed');
+       // });
+       // Promise.race([
+       //  doNewChallenge,
+       //  doChallengeTwo,
+       //  doChallengeThree
+       // ]).then((message) => {
+       //  console.log(message);
+       // })
 
 function makeRequest(location){
        return new Promise((resolve, reject) => {
@@ -101,11 +101,24 @@ function processRequest(response){
               resolve(`extra information ${response}`)
        })
 }
-makeRequest('facebook').then(response =>{
-       console.log('response received')
-      return processRequest(response);
-}).then(processResponse =>{
-       console.log(processResponse);
-}).catch(err =>{
-       console.log(err)
-})
+// makeRequest('facebook').then(response =>{
+//        console.log('response received')
+//       return processRequest(response);
+// }).then(processResponse =>{
+//        console.log(processResponse);
+// }).catch(err =>{
+//        console.log(err)
+// })
+
+async function doWork(){
+       try{
+              const response = await makeRequest('facebook');
+  console.log('response received');
+  const processResponse = await processRequest(response)
+  console.log(processResponse);
+       } catch(err){
+              console.log(err)
+       }
+  
+}
+doWork()
