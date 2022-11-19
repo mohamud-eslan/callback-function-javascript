b06d7f77966ca6d9e2e2012604ddc855df089327
mohamud-eslan <mohmud141@gmail.com>
2022-11-19 05:15:34 +0300

    hoisting in js

---
+++ hoisting.js
@@ -0,0 +1,19 @@
+
+const initApp = () => {
+    console.log(stepOne)
+    stepOne();
+
+}
+
+const stepOne = () => {
+    console.log('step one');
+}
+
+const sumConst = (a, b) => a + b;
+function sumFuntion(a, b){
+    return a + b;
+}
+
+console.log(`sumFunction(1, 2) => ${sunFunction(1, 2)}`);
+console.log(`sumConst(3, 4) => ${sumConst(3, 4)}`);
+
